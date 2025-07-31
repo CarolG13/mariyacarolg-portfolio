1711b02b20713332b20fcc2e5d8bc9e92d8ee35b
CarolG13 <carolcarol04448@gmail.com>
2025-07-31 20:31:42 +0530

    step3 : Create hero section and personal data file

---
+++ src/data/site.ts
@@ -0,0 +1,12 @@
+// src/data/site.ts
+export const SITE_TITLE = 'Mariya Carol G | BCA Graduate , Data Science & AI';
+export const SITE_DESCRIPTION = 'The personal portfolio for Mariya Carol G, a BCA Graduate.';
+export const PERSONAL_INFO = {
+  name: 'Mariya Carol G',
+  tagline: 'A Computer Applications Graduate Passionate about Data Science & AI Technologies',
+  bio: `I've recently completed my BCA where I discovered a strong interest in the power of Data Science and Artificial Intelligence.I enjoy learning how to find patterns in data and am eager to build .I'm a dedicated and curious learner looking for my first professional opportunity as a data analyst or junior data scientist.`,
+  location: 'Bengaluru, Karnataka, India',
+  email: 'carolcarol04448@gmail.com',
+  github_handle: 'CarolG13',
+  linkedin_handle: 'mariya-carol-g-130212ade',
+};
